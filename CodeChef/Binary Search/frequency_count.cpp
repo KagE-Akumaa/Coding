fbd699ccb608568949e557495ee786ab0a25d581
KagE-Akumaa <mukuljogi18@gmail.com>
2025-04-28 10:14:57 +0530

    Day-9

---
+++ CodeChef/Binary Search/frequency_count.cpp
@@ -0,0 +1,22 @@
+// Given a sorted array which can have repeated elements and an element k we need to find the frequency of a given element
+// eg .. 0 1 1 1 2 2 2 3 4 4 5 10
+
+#include <bits/stdc++.h>
+#define int long long int
+using namespace std;
+
+void solve()
+{
+    cout << "hello" << endl;
+}
+
+int32_t main() {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	
+	int t = 1;
+	cin >> t;
+	while(t--)
+		solve();
+	return 0;
+}
